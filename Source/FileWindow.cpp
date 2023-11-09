1e0022669f2620846b9f754c6daf2fdc4cd78e17
Vincent Huang <chenzhuh@andrew.cmu.edu>
2023-11-08 19:58:55 -0500

    Add playback ability

--- Source/FileWindow.cpp
+++ Source/FileWindow.cpp
@@ -1,20 +1,70 @@
 #include "FileWindow.h"
 
 //==============================================================================
-FileWindow::FileWindow()
+FileWindow::FileWindow(): state(Stopped)
 {
     setName("File Window");
     setOpaque(true);
     setInterceptsMouseClicks(true, true);
     
-    addAndMakeVisible(playButton);
+    addAndMakeVisible(&playButton);
     playButton.setButtonText("Play Audio");
-    addAndMakeVisible(convertButton);
+    playButton.onClick = [this] {
+        playButtonClicked();
+    };
+    playButton.setEnabled(false);
+    
+    addAndMakeVisible(&stopButton);
+    stopButton.setButtonText("Stop Audio");
+    stopButton.onClick = [this] {
+        stopButtonClicked();
+    };
+    stopButton.setEnabled(false);
+    
+    addAndMakeVisible(&convertButton);
     convertButton.setButtonText("Convert to MIDI");
+    convertButton.setEnabled(false);
+    
+    formatManager.registerBasicFormats(); // register the basic audio file formats, wav, aiff files
+    transportSource.addChangeListener(this); // when there is a change in the transportSource, we trigger the changeListenerCallback function
+    setAudioChannels(0, 2);
 }
 
 FileWindow::~FileWindow()
 {
+    shutdownAudio();
+}
+
+void FileWindow::playButtonClicked() {
+    changeState(Starting);
+}
+
+void FileWindow::stopButtonClicked() {
+    changeState(Stopping);
+}
+
+void FileWindow::prepareToPlay(int samplesPerBlockExpected, double sampleRate) {
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void FileWindow::getNextAudioBlock(const juce::AudioSourceChannelInfo &bufferToFill) {
+    if (readerSource.get() == nullptr) {
+        bufferToFill.clearActiveBufferRegion();
+        return;
+    }
+    
+    transportSource.getNextAudioBlock(bufferToFill);
+}
+
+void FileWindow::releaseResources() {
+    transportSource.releaseResources();
+}
+
+void FileWindow::changeListenerCallback(juce::ChangeBroadcaster *source) {
+    if (source == &transportSource) {
+        if (transportSource.isPlaying()) changeState(Playing);
+        else changeState(Stopped);
+    }
 }
 
 bool FileWindow::isInterestedInFileDrag(const juce::StringArray& files) {
@@ -23,16 +73,49 @@ bool FileWindow::isInterestedInFileDrag(const juce::StringArray& files) {
 }
 
 void FileWindow::filesDropped(const juce::StringArray& filenames, int x, int y) {
-    juce::Array<juce::File> files;
-    std::cout << "file dropped" << std::endl;
-    
-    for (auto& f: filenames) {
-        files.add({ f });
+    if (filenames.size() != 1) {
+        juce::Logger::writeToLog("Invalid format of file dropped, skipping");
+        return;
     }
+    droppedFile = juce::File(filenames[0]); // store the file path
+    juce::Logger::writeToLog("File Dropped: " + droppedFile.getFullPathName());
+
+    auto* reader = formatManager.createReaderFor(droppedFile);
+    juce::Logger::writeToLog("Reader initialized");
     
-    for (auto& file: files) {
-        auto content = file.loadFileAsString();
-        std::cout << "File content: " << content << std::endl;
+    if (reader != nullptr) {
+        auto newSource = std::make_unique<juce::AudioFormatReaderSource> (reader, true);
+        transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+        juce::Logger::writeToLog("Activate playback button");
+        playButton.setEnabled(true);
+        readerSource.reset(newSource.release());  // assign the ownership of the resource to the class field so that the memory doesn't get released when this function finishes running
+    }
+}
+
+void FileWindow::changeState(TransportState newState) {
+    if (state != newState) {
+        state = newState;
+        
+        switch (state) {
+            case Stopped:
+                stopButton.setEnabled(false);
+                playButton.setEnabled(true);
+                transportSource.setPosition(0.0);  // reset the position
+                break;
+                
+            case Starting:
+                playButton.setEnabled(false);
+                transportSource.start();  // start playing
+                break;
+                
+            case Playing:
+                stopButton.setEnabled(true);
+                break;
+            
+            case Stopping:
+                transportSource.stop();
+                break;
+        }
     }
 }
 
@@ -40,7 +123,6 @@ void FileWindow::fileDragMove(const juce::StringArray &files, int x, int y) {}
 
 void FileWindow::fileDragExit(const juce::StringArray &files) {}
 
-//==============================================================================
 void FileWindow::paint (juce::Graphics& g)
 {
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
@@ -57,6 +139,7 @@ void FileWindow::paint (juce::Graphics& g)
     g.drawText ("Drop your audio file here!", fileDropArea, juce::Justification::centred, true);
     
     auto controlRow = getLocalBounds().removeFromBottom(1.0 / 5.0 * getHeight());
-    playButton.setBounds(controlRow.removeFromLeft(getWidth() * 0.5));
-    convertButton.setBounds(controlRow.removeFromLeft(getWidth() * 0.5));
+    playButton.setBounds(controlRow.removeFromLeft(getWidth() / 3.0));
+    stopButton.setBounds(controlRow.removeFromLeft(getWidth() / 3.0));
+    convertButton.setBounds(controlRow.removeFromLeft(getWidth() / 3.0));
 }
--- Source/FileWindow.h
+++ Source/FileWindow.h
@@ -3,20 +3,59 @@
 #include <JuceHeader.h>
 
 
-class FileWindow  : public juce::Component, public juce::FileDragAndDropTarget
+
+class FileWindow: public juce::AudioAppComponent,
+                  public juce::FileDragAndDropTarget,
+                  public juce::ChangeListener
 {
 public:
     FileWindow();
     ~FileWindow() override;
 
     void paint (juce::Graphics&) override;
+    
+    // FileDragAndDropTarget functions
     bool isInterestedInFileDrag (const juce::StringArray&) override;
     void filesDropped (const juce::StringArray&, int, int) override;
     void fileDragMove(const juce::StringArray& files, int x, int y) override;
     void fileDragExit(const juce::StringArray& files) override;
+    
+    // AudioAppComponent functions
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo &bufferToFill) override;
+    void releaseResources() override;
+    
+    // ChangeListener functions
+    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
+    
 
 private:
+    // Playback buttons
     juce::TextButton playButton;
     juce::TextButton convertButton;
+    juce::TextButton stopButton;
+    
+    void playButtonClicked();
+    void stopButtonClicked();
+//    void convertButtonClicked();
+    
+    // Audio-playback-related modules
+    enum TransportState
+    {
+        Stopped,
+        Starting,
+        Playing,
+        Stopping
+    };
+    void changeState(TransportState newState);
+
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+    TransportState state;
+
+    // I/O
+    juce::File droppedFile;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FileWindow);
 };
